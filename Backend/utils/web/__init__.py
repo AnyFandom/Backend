87b024b950e2b8a8b5da8265ee653cd44aa0e9d1
Rijenkii <aleksandr_koshkarev@hotmail.com>
2017-02-11 20:58:01 +0700

    Немножко убрал все из __init__'ов

--- Backend/utils/web/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from . import exceptions
-from . import middlewares
-from .rewrites import BaseView
-from .response import JsonResponse
--- Backend/views/root.py
+++ Backend/views/root.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from ..utils import web
+from ..utils.web.rewrites import BaseView
+from ..utils.web.response import JsonResponse
 
 
-class RootView(web.BaseView):
+class RootView(BaseView):
     async def get(self):
         resp = await self.request.conn.fetchrow(
             "SELECT 'testing testing 123' AS test")
 
-        return web.JsonResponse({'test': resp['test']})
+        return JsonResponse({'test': resp['test']})
