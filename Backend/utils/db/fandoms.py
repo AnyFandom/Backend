6c40ae29f213941209d1d467e1fe9cabfd6be29c
Rijenkii <aleksandr_koshkarev@hotmail.com>
2017-03-18 23:34:50 +0700

    Добавил add и get в db.fandoms

---
+++ Backend/utils/db/fandoms.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from typing import Union
+
+import asyncpg
+
+_sqls = {'add': "SELECT fandoms_create($1, $2, $3, $4, $5)",
+         'get': "SELECT * FROM fandoms"}
+
+
+def _format(obj):
+    return dict(
+        type='fandoms',
+        id=obj['id'],
+        attributes=dict(
+            created_at=obj['created_at'],
+            edited_at=obj['edited_at'],
+            edited_by=obj['edited_by'],
+            title=obj['title'],
+            url=obj['url'],
+            description=obj['description'],
+            avatar=obj['avatar'],
+            owner=obj['owner']
+        )
+    )
+
+
+async def add(conn: asyncpg.connection.Connection,
+              uid: int, title: str, url: str, descr: str, avatar: str) -> int:
+    return await conn.fetchval(_sqls['add'], uid, url, title, descr, avatar)
+
+
+async def get(conn: asyncpg.connection.Connection,
+              *ids: Union[str, int], u: bool=False) -> list:
+    if u and ids:
+        resp = await conn.fetch(
+            _sqls['get'] + " WHERE url = ANY($1::CITEXT[])", ids)
+    elif ids:
+        resp = await conn.fetch(
+            _sqls['get'] + " WHERE id = ANY($1::BIGINT[])", ids)
+    else:
+        resp = await conn.fetch(_sqls['get'])
+
+    return [_format(x) for x in resp]
